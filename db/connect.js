0349859fc7fce759bdc1b36e78dd964461e3d6c8
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2022-12-21 12:38:01 +0630

    connect to DB

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
+  mongoose.set('strictQuery', false);
+  return mongoose.connect(url);
+};
 
-module.exports = connectDB
+module.exports = connectDB;
