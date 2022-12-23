3ab460e008d033c9968f703728f9ae2150e97658
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2022-12-23 12:21:22 +0630

    check user & login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { StatusCodes } = require('http-status-codes');
 const User = require('../models/User');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -13,7 +14,29 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('Login User');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  // Check user
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError('Invalid credentials!');
+  }
+
+  // Check password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid credentials!');
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: { name: user.name },
+    token,
+  });
 };
 
 module.exports = { register, login };
--- models/User.js
+++ models/User.js
@@ -33,9 +33,19 @@ UserSchema.pre('save', async function () {
 
 // Create JWT using instance methods
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, 'jwtSecret', {
-    expiresIn: '30d',
-  });
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES,
+    }
+  );
+};
+
+// Check password
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
 };
 
 module.exports = mongoose.model('User', UserSchema);
