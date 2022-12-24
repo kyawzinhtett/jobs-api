4e165a036769fdceb2d3d2296103ec87d0ccefa3
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2022-12-24 12:00:54 +0630

    get single job

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -13,7 +13,21 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send('Get Job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job found with this id: ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
