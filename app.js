6f026826f44548ecd9dc435f4d3fc970e4795706
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2022-12-27 17:12:00 +0630

    set extra security packages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 require('express-async-errors');
+// Extra Security Packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
 const app = express();
 
 // Connect DB
@@ -17,8 +23,19 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
